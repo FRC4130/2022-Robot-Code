b1068c7e0e2321d1c7abbc8a711b5c76f68f8cae
FlamemasterNXF <flamemasternxf@gmail.com>
2022-02-05 14:11:48 -0500

    gradlew

--- 2022-CompBot-Code/src/main/java/frc/robot/Robots/RobotMap.java
+++ 2022-CompBot-Code/src/main/java/frc/robot/Robots/RobotMap.java
@@ -5,9 +5,10 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 public class RobotMap {
     public static TalonFX leftDrive;
     public static TalonFX leftDrive2;
-
     public static TalonFX rightDrive;
     public static TalonFX rightDrive2;
+
+    //public static CANSparkMax neo;
 
     public static void Init(){
         leftDrive = new TalonFX(1);
