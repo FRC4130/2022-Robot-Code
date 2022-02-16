054f7e482ed1db8575767d4505550d4fd8ca121c
Courtney <henrycourtney9279@gmail.com>
2022-02-15 19:09:53 -0500

    Added Climb.java & ClimbTele.java (not blank hopefully)

---
+++ 2022-CompBot-Code/src/main/java/frc/robot/Loops/ClimbTele.java
@@ -0,0 +1,37 @@
+package frc.robot.Loops;
+
+import com.ctre.phoenix.ILoopable;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
+import edu.wpi.first.wpilibj.PS4Controller;
+import frc.robot.Robots.RobotMap;
+import frc.robot.Robots.Subsystems;
+import frc.robot.Subsystems.Climb;
+
+public class ClimbTele implements ILoopable{
+
+    Climb _operateClimb;
+    PS4Controller _opcontroller;
+
+    public ClimbTele(){
+        _operateClimb = Subsystems.climb;
+        _opcontroller = RobotMap.opController;
+    }
+
+    public void onStart(){
+        _operateClimb.setNeutralMode(NeutralMode.Brake);
+    }
+
+    public void onLoop(){
+        _operateClimb.ClimbMovement(_opcontroller.getLeftY(), _opcontroller.getRightY());
+    }
+
+    public boolean isDone(){
+        return false;
+    }
+
+    public void onStop(){
+        _operateClimb.ClimbMovement(0, 0);
+    }
+
+}
